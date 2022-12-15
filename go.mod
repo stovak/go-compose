4ee772a3aeca0599eb5bc62f6731b4818e4eacaf
Tom Stovall <stovak@gmail.com>
2022-12-14 21:06:48 -0800

    building out go framework

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/stovak/go-compose
+
+go 1.19
--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
-	
+	// get file from terminal
+	inputFile := os.Args[1]
+	// read the whole content of file and pass it to file variable, in case of error pass it to err variable
+	file, err := os.ReadFile(inputFile)
+	if err != nil {
+		fmt.Printf("Could not read the file due to this %s error \n", err)
+	}
+	// convert the file binary into a string using string
+	fileContent := string(file)
+	// print file content
+	fmt.Println(fileContent)
 }
