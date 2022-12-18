ac9fd0e635489f5ea496ed17e600cf3f72ecd208
Tom Stovall <stovak@gmail.com>
2022-12-17 19:27:08 -0800

    wip

--- main.go
+++ main.go
@@ -2,20 +2,14 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"github.com/stovak/go-composer/pkg/composer"
+	"os"
 )
 
 func main() {
 	// get file from terminal
-	inputFile := os.Args[1]
 	// read the whole content of file and pass it to file variable, in case of error pass it to err variable
-	file, err := os.ReadFile(inputFile)
-	if err != nil {
-		fmt.Printf("Could not read the file due to this %s error \n", err)
-	}
-	// convert the file binary into a string using string
-	fileContent := composer.ComposerFile(file)
+	parsedFile := composer.New(os.Args[1])
 	// print file content
-	fmt.Println(fileContent)
+	fmt.Printf("Parsed: %+v", parsedFile)
 }
--- pkg/composer/file.go
+++ pkg/composer/file.go
@@ -1,7 +1,31 @@
 package composer
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
 type File struct {
-	name string `mapstructure:"name"`
-	description string `mapstructure:"description"`
-    version string `mapstructure:"version"`
+	Name        string `mapstructure:"name"`
+	Description string `mapstructure:"description"`
+	Version     string `mapstructure:"version"`
+}
+
+func New(filename string) File {
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		log.Fatal("Error when opening file: ", err)
+	}
+
+	// Now let's unmarshall the data into `payload`
+	var payload File
+	err = json.Unmarshal(content, &payload)
+	if err != nil {
+		log.Fatal("Error during Unmarshal(): ", err)
+	}
+
+	// Let's print the unmarshalled data!
+	log.Printf("payload: %+v", payload)
+	return payload
 }
